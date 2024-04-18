7e50b2fed01f3c664c9336582934d2c78311dea9
Anthony Shook <anthonyshook@gmail.com>
2024-04-18 10:01:32 -0500

    dashboard updates

--- server.R
+++ server.R
@@ -13,6 +13,7 @@ library(data.table)
 library(ggplot2)
 library(plotly)
 library(bslib)
+library(ggtext)
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
@@ -35,8 +36,6 @@ function(input, output, session) {
   
   ##### UI ELEMENTS #####
   output$band_select_ui <- renderUI({
-    # selectInput(inputId = 'band_select', label = 'Pick a Band / Artist!', choices = sort(unique(dat$Band)), multiple = F, 
-    #             selected = sample(unique(dat$Band), size = 1), selectize = T)
     shinyWidgets::virtualSelectInput(
       inputId = "band_select",
       label = "Pick a Band / Artist!",
@@ -51,11 +50,14 @@ function(input, output, session) {
   
   
   ##### OBSERVE #####
-  # observe(input$band_select, {
+  # observeEvent(input$band_select, {
   #   sidebar_toggle(
   #     id = "band_select_sidebar"
   #   )
-  #   })
+  #   }, ignoreInit = TRUE, ignoreNULL = TRUE)
+  # observe(session$setCurrentTheme(
+  #   if (isTRUE(input$dark_mode)) dark else light
+  # ))
   
   ##### REACTIVES #####
   dat_band_summary <- reactive({
@@ -82,28 +84,40 @@ function(input, output, session) {
   ##### DATA ELEMENTS #####  
   output$band_summary <- renderReactable({
     reactable(dat_band_summary(),
+              class = 'tables',
+              columns = list(
+                ShowID = colDef(show  = FALSE),
+                BandID = colDef(show = FALSE)
+              ),
               filterable = F, searchable = F, fullWidth = TRUE, compact = TRUE, pagination = F)
   })
   
   output$played_with <- renderReactable({
     reactable(dat_band_played_with(),
+              class = 'tables',
+              columns = list(
+                ShowID = colDef(show = FALSE),
+                BandID = colDef(show = FALSE)
+              ),
               filterable = F, searchable = F, fullWidth = TRUE, compact = TRUE, pagination = F)
   })
   
   output$show_list <- renderReactable({
     reactable(dat_show_list(),
+              class = 'tables',
               columns = list(
                 Date = colDef(maxWidth = 100),
                 Bands = colDef(maxWidth = 100),
                 Venue = colDef(maxWidth = 500)
               ),
-              defaultSorted = list('Venue' = 'asc'),
-              filterable=T, searchable = T, fullWidth = T, compact = T, pagination = F, striped=T)
+              defaultSorted = list('Date' = 'asc'),
+              filterable=F, searchable = T, fullWidth = T, compact = T, pagination = F, striped=T)
   })
   
   
   output$raw_data <- renderReactable({
-    rtable <- reactable(dat,
+    rtable <- reactable(dat, 
+                        class = 'tables',
                         defaultSorted = list('ShowID' = 'desc'),
                         filterable = FALSE, 
                         searchable = TRUE,
@@ -129,23 +143,54 @@ function(input, output, session) {
     plotdata <- band_summary[order(-`Number of Shows`, -most_recent_date)][1:10]
     newPlot <- rbindlist(lapply(1:10, function(z) {data.table(Band = rep(plotdata$Band[z], times = plotdata$`Number of Shows`[z]))}))
     newPlot[, Shows := 1:.N, by=Band]
+    newPlot[, height := 1]
     newPlot$Band <- factor(x = newPlot$Band, levels = plotdata$Band, ordered = TRUE)
     
-    ggplot(newPlot, aes(x = Shows, y = Band)) + 
-      geom_tile(aes(fill=Shows, width=0.9, height=0.9)) + 
-      scale_y_discrete(limits = rev) + 
-      # scale_fill_manual(values=colours) + 
+    # ggplot(newPlot, aes(x = height, y = Band, fill = Shows)) + 
+    #   geom_col(position=position_stack()) +
+    #   scale_y_discrete(limits = rev) + 
+    #   labs(x='', y='', title='Most Seen Bands') +
+    #   geom_textbox(fill=NA, 
+    #                aes(label = paste0("<span style=font-size:12pt>", Band, "</span>"),
+    #                    text.colour='white',
+    #                    box.colour=NA,
+    #                    fontface='bold',
+    #                    halign=1,
+    #                    hjust=.75,
+    #                    size = 16),
+    #   ) +
+    #   theme(axis.title = element_blank(), 
+    #         axis.ticks = element_blank(),
+    #         axis.text.y = element_blank(),
+    #         text = element_text(colour='#EEEEEE', size = 16),
+    #         axis.text = element_text(colour='#EEEEEE', size = 12),
+    #         panel.grid = element_blank(),
+    #         legend.position = "none",
+    #         panel.border = element_blank(),
+    #         panel.background = element_rect(fill='#3E444F'), #transparent panel bg
+    #         plot.background = element_rect(fill='#3E444F', color=NA) #transparent plot bg
+    #   ) +
+    #   scale_fill_gradient2(low = '#76ABAE', mid = '#76ABAE', high = '#76ABAE')
+    
+    ggplot(newPlot, aes(x = Shows, y = Band)) +
+      geom_tile(aes(fill=Shows, width=0.9, height=0.9)) +
+      scale_y_discrete(limits = rev) +
+      # scale_fill_manual(values=colours) +
       labs(title = "Most Seen Bands") +
-      theme_minimal(16) + 
-      scale_fill_gradient(low="black", high="darkgreen") +
-      theme(axis.title.x = element_blank(), 
+      theme_minimal(16) +
+      scale_fill_gradient(low="#222831", high="#76ABAE") +
+      theme(axis.title.x = element_blank(),
             axis.title.y = element_blank(),
             axis.ticks.x = element_blank(),
             axis.ticks.y = element_blank(),
+            text = element_text(colour='#EEEEEE'),
+            axis.text = element_text(colour='#EEEEEE', size = 12),
             panel.grid = element_blank(),
             legend.position = "none",
-            panel.background = element_rect(fill = 'white', color = 'white')
+            panel.background = element_rect(fill='#3E444F'), #transparent panel bg
+            plot.background = element_rect(fill='#3E444F', color=NA) #transparent plot bg
       )
+
   })
   
   # Top 10 Venue Graph
@@ -165,9 +210,12 @@ function(input, output, session) {
             axis.title.y = element_blank(),
             axis.ticks.x = element_blank(),
             axis.ticks.y = element_blank(),
+            text = element_text(colour='#EEEEEE'),
+            axis.text = element_text(colour='#EEEEEE', size=12),
             panel.grid = element_blank(),
             legend.position = "none",
-            panel.background = element_rect(fill = 'white', color = 'white')
+            panel.background = element_rect(fill='#3E444F'), #transparent panel bg
+            plot.background = element_rect(fill='#3E444F', color=NA) #transparent plot bg
       )
   })
   
@@ -179,16 +227,19 @@ function(input, output, session) {
     
     ggplotly(
       ggplot(plotdata, aes(x = Year, y = Month)) +
-        geom_tile(aes(fill = Shows, width=.8, height=.8, color='white')) + 
+        geom_tile(aes(fill = Shows, width=.8, height=.8, color='transparent')) + 
         scale_y_discrete(limits = rev) + 
         scale_fill_gradient(low="black", high="purple") +
-        theme_bw() +
+        theme_bw(14) +
         theme(axis.title.x = element_blank(), 
               axis.title.y = element_blank(),
               axis.ticks = element_blank(),
               panel.grid = element_blank(),
               legend.position = "none",
-              panel.background = element_rect(fill = 'white', color = 'white')
+              text = element_text(colour='#EEEEEE'),
+              axis.text = element_text(colour='#EEEEEE'),
+              panel.background = element_rect(fill='transparent'), #transparent panel bg
+              plot.background = element_rect(fill='transparent', color=NA) #transparent plot bg
         ),
       tooltip = c('x', 'y', 'fill')
     ) %>% layout(xaxis = list(fixedrange = TRUE), yaxis = list(fixedrange = TRUE)) %>% config(displayModeBar = F)
--- ui.R
+++ ui.R
@@ -13,68 +13,90 @@ library(plotly)
 library(bslib)
 library(waiter)
 
+# Setup Colors ------------------------------------------------------------
+dark_list <- list(
+  col_accent_pop = '#76ABAE',
+  col_light = '#EEEEEE',
+  col_accent_focus = '#222831',
+  col_main_body = '#3E444F',
+  col_bg = '#31363F',
+  col_fg = '#eeeeee',
+  col_navbar = '#222831'
+)
+
+# light theme
+light <- bs_theme()
+
+# Dark theme
+dark <- bs_theme(bg = dark_list$col_bg, fg = dark_list$col_light) |> 
+  bs_add_rules(sass::as_sass(dark_list)) |>
+  bs_add_rules(sass::sass_file("./www/dash.scss"))
+
+
+# SHINY CODE --------------------------------------------------------------
 page_navbar( 
-  # title = tagList(
-  #   tags$a(bsicons::bs_icon('github'), "Get the Source Code!",
-  #          href='https://github.com/anthonyshook/bandHistoryApp',
-  #          target='_blank', style = 'color:white;')),
-  inverse = TRUE,
+  lang='en',
+  window_title='Live Show Dashboard',
+  inverse = 'auto',
+  bg=dark_list$col_navbar,
   fillable = TRUE, 
-  footer = tagList(
-    
-    waiting_screen <- tagList(
-      spin_wave()
-    ),
-    autoWaiter(html = waiting_screen, color = 'rgba(84, 89, 95, .75)'),
-    waiter_preloader(html = waiting_screen, color = '#54595F')
-  ),
-  # sidebar = nav,
-  # THEME -------------------------------------------------------------------
-  ## Build a theme to include, perhaps
-  theme = bslib::bs_theme(base_font = bslib::font_collection(bslib::font_google('Roboto', local=TRUE), "sans-serif")),
   
   # WAITER LOADING OPTIONS --------------------------------------------------
-  # This is used to include "Loading" options to 
-  
+  # Placed quietly in footer -- currently not working, because it just creates a weird effect.
+  footer = tagList(
+    div(class = 'dont-show',
+        waiting_screen <- tagList(
+          spin_wave()
+        ),
+        #autoWaiter(html = waiting_screen, color = 'rgba(84, 89, 95, .75)'),
+        use_waiter(),
+        waiter_preloader(html = waiting_screen, color = dark_list$col_navbar)
+    )
+  ),
+  # THEME -------------------------------------------------------------------
+  ## Build a theme to include, perhaps
+  # base_font = bslib::font_collection(bslib::font_google('Nunito', local=TRUE), "sans-serif")
+  theme = dark,
   
   # FRONT-END CODE START ----------------------------------------------------
-  
+  # checkboxInput('dark_mode', 'Dark Mode'),
   nav_spacer(),
-  
   nav_panel('Summary',
             layout_columns(
-              value_box("Unique Bands", value = textOutput('num_bands')),
-              value_box("Unique Shows*", value = textOutput('num_shows')),
-              value_box("Unique Venues*", value = textOutput('num_venues'))
+              value_box(title = NULL, value = textOutput('num_bands'), h5('Unique Bands'), class = 'summaryCard'),
+              value_box(title = NULL, value = textOutput('num_shows'), h5("Unique Shows*"), class = 'summaryCard'),
+              value_box(title = NULL, value = textOutput('num_venues'), h5("Unique Venues*"), class = 'summaryCard')
             ),
             layout_columns(
-              card(plotOutput('top10_bandGraph')),
-              card(plotOutput('top10_venuesGraph'))
+              card(plotOutput('top10_bandGraph'), class='graphs'),
+              card(plotOutput('top10_venuesGraph'), class='graphs')
             ),
             layout_columns(
-              card(plotlyOutput('timeline'))
+              card(plotlyOutput('timeline'), class='graphs')
             )
   ),
-  nav_panel('By Band',
+  nav_panel('Explore By Band',
             layout_sidebar(border = TRUE, 
                            sidebar = sidebar(width = '20%',
                                              id='band_select_sidebar',
+                                             open=list(desktop='always', 
+                                                       mobile='closed'),
                                              uiOutput('band_select_ui')
                            ), 
-                           card(
-                             h3('Shows'),
-                             reactableOutput('band_summary'),
-                             hr(),
-                             h3('Seen with...'),
-                             reactableOutput('played_with')
+                           card(class='tables',
+                                h3('Shows'),
+                                reactableOutput('band_summary'),
+                                hr(),
+                                h3('Seen with...'),
+                                reactableOutput('played_with')
                            )
                            
             )
   ), 
-  nav_panel('By Show', 
-            reactableOutput('show_list')),
+  nav_panel('Explore By Show', 
+            card(class='tables', reactableOutput('show_list'))),
   nav_panel('Totally Raw Data', 
-            reactableOutput('raw_data')),
+            card(class='tables', reactableOutput('raw_data'))),
   nav_item(
     tagList(
       tags$a(bsicons::bs_icon('github'), "Get the Source Code!",
